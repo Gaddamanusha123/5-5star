91c780b0367546698d3138075acd7391ee833f87
Gaddamanusha123 <anusha6113@gmail.com>
2025-05-14 12:59:53 +0530

    Add files via upload

---
+++ 5-5.py
@@ -0,0 +1,10 @@
+rows=5
+mid=rows//2+1
+for i in range(1,rows+1):
+    res=" "
+    for j in range(1,rows+1):
+        if i==1 or i==rows or j==1 or j==rows or i==mid and j==mid:
+                res+="*"+" "
+        else:
+             res+=" "+" "
+    print(res)
